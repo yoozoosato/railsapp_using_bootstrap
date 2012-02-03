6a80d879bf9fcc137d35748be17c9a6bf093cc49
SATO Yozo <yoozoosato@gmail.com>
2012-02-03 17:52:07 +0900

    新規にRailsアプリを作成
    3.2.1 を bundle するところで苦労したけど、無事に作成完了

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '307d41bbd4633524026778284a9b7af2c8a4fb9362e0c4da000e0993da8a3454779fc44cb8a3a868ffae8d8dd411236bef04114971ac072423e551367e166554'
